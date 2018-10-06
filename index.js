1d231b1a773c3afacc34670c168f1199f4f89069
j o'connor <j o'connor>
2018-10-06 23:07:49 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/** @format */
+
+//import a library to help create a component
+
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+//create a component
+
+const App = () =>	(
+	<View>
+		<Header headerText={'Albums'} />
+		<AlbumList />
+	</View>
+		);
+
+
+//render it to the device
+
+AppRegistry.registerComponent('albums', () => App);
